c6f311dcf8bc5e6edbbbd153d92a4c24841ab755
mopsprivet <nastya.sitchenko@mail.ru>
2024-04-22 16:04:44 +0500

    Test sessions

--- NetLibBack/src/main/java/com/JavaEE/netlib/controller/UserController.java
+++ NetLibBack/src/main/java/com/JavaEE/netlib/controller/UserController.java
@@ -20,13 +20,41 @@ public class UserController {
 
     @PostMapping(path="/register")
     public void createUser(@RequestBody User user) throws Exception {
-        userService.saveUser(user);
+        userService.registerUser(user);
     }
 
-    @PostMapping(path="/login")
-    public void loginUser(@RequestBody User user) throws Exception {
-        userService.loginUser(user);
+//    @PostMapping(path="/login")
+//    public void loginUser(@RequestBody User user) throws Exception {
+//        userService.loginUser(user);
+//    }
+
+//    @PostMapping("/login")
+//    public ResponseEntity<?> loginUser(@RequestBody User user, HttpSession session) {
+//        if (userService.loginUser(user.getUsername(), user.getPassword())) {
+//            session.setAttribute("username", user.getUsername());
+//            return ResponseEntity.ok("User authenticated successfully");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+//        }
+//    }
+//
+//    // инфа о текущем пользователе из сессии
+//    @GetMapping("/userInfo")
+//    public ResponseEntity<?> getCurrentUser(HttpSession session) {
+//        String username = (String) session.getAttribute("username");
+//        if (username != null) {
+//            return ResponseEntity.ok("Current user: " + username);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
+//        }
+//    }
+//
+//    @PostMapping("/logout")
+//    public ResponseEntity<?> logoutUser(HttpSession session) {
+//        session.invalidate();
+//        return ResponseEntity.ok("User logged out successfully");
+//    }
 
 
-    }
+
 }
